363df7b27f77b58439e2f66f123b5acad991d8fa
Ahmed Balti <Ahmed Balti@DESKTOP-JNHVTQV>
2022-05-06 09:05:35 +0100

    Ajoutez des fichiers projet.

---
+++ SpecFlowFormation/Features/ContentSubmission.feature
@@ -0,0 +1,9 @@
+﻿Feature: ContentSubmission
+
+A short summary of the feature
+
+@tag1
+Scenario: Tiltle is set to specflow Community Content Submission
+
+	When the submission page is open
+	Then the title of the page is 'SpecFLow Cimmunity Content Submission'
---
+++ SpecFlowFormation/StepDefinitions/WebPagesSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowFormation.StepDefinitions
+{
+    [Binding]
+    public class WebPagesSteps
+    {
+        [When(@"the submission page is open")]
+        public void WhenTheSubmissionPageIsOpen()
+        {
+            throw new PendingStepException();
+        }
+
+
+        [Then(@"the title of the page is '([^']*)'")]
+        public void ThenTheTitleOfThePageIs(string pageTitle)
+        {
+            throw new PendingStepException();
+        }
+
+
+    }
+}
